c3b4df8928ddef03638737546dbc00251826e73a
amercado <arnold.mercado@hotmail.com>
2025-02-02 19:04:29 +0800

    Improve ProductCard

--- src/app/(root)/(home)/_components/ProductCard.tsx
+++ src/app/(root)/(home)/_components/ProductCard.tsx
@@ -12,17 +12,22 @@ const ProductCard = ({data,onCardClick}: ProductProps) => {
 
   return (
     <Card
-        className="flex w-full items-center flex-col px-4 py-4 cursor-pointer"
+        className="flex w-full items-center flex-col  cursor-pointer overflow-hidden"
         onClick={() => onCardClick && onCardClick(data._id)}
     >
+
       <Image
         src={`/images/${data.image}`}
         alt={data.title}
         width={100}
         height={400}
-        className="object-cover w-full  rounded-lg"/>
-      <p className="p-4 text-base ">{data.title}</p>
-      <p>Price: {data.price} Pesos</p>
+        className="object-fill w-full "/>
+      <div className="w-full px-4 py-2">
+        <p className="text-base ">{data.title}</p>
+        <p className="flex-between w-full">
+          <span>Price:</span>
+          <span>{data.price} Pesos</span></p>
+      </div>
     </Card>
   )
 }
